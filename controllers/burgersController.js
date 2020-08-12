173c7791ac11469548c1e726d4d1a10718096247
t.sarker <tanmay.sarker@baruchmail.cuny.edu>
2020-08-12 18:10:59 -0400

    updated

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -2,10 +2,8 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+var burger = require("../models/burgers.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
   burger.all(function (data) {
     var hbsObject = {
@@ -21,8 +19,9 @@ router.post("/api/burgers", function (req, res) {
     ["name", "devoured"],
     [req.body.name, req.body.devoured],
     function (result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
+      res.json({
+        id: result.insertId
+      });
     }
   );
 });
@@ -32,14 +31,12 @@ router.put("/api/burgers/:id", function (req, res) {
 
   console.log("condition", condition);
 
-  burger.update(
-    {
+  burger.update({
       devoured: req.body.devoured,
     },
     condition,
     function (result) {
       if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO burgers (name) VALUES ('Meeses');
-INSERT INTO burgers (name) VALUES ('Bobbi');
-INSERT INTO burgers (name) VALUES ('Sylvester');
-INSERT INTO burgers (name) VALUES ('Marilyn Manson');
-INSERT INTO burgers (name) VALUES ('Joe Biden');
-INSERT INTO burgers (name) VALUES ('Owens');
+USE burgers_db;
+
+INSERT INTO burgers (name)
+VALUES ('Beyond Burger'), ('Impossible Burger'), ('Bean Burger'), ('Veggie Burger');
